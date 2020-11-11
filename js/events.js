ad44adb02861b23fb8e56f4fa30208e088421bfd
miShelbyT <mi.shelbyrose@gmail.com>
2020-11-11 18:12:44 -0500

    fun lab

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+	$('p').on('click', () => {
+		alert('hey!');
+	})
+}
+
+function frameIt() {
+  $(window).on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $(window).on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 71){
+			alert('GGGG was pressed');
+		}
+  })
+}
+
+
 
 $(document).ready(function(){
 
+	getIt()
+	frameIt()
+	submitIt()
+	pressIt()
 // call functions here
 
 });
